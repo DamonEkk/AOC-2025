eabd26031e8e5d22087f702e79576e26ff1cc259
DamonEkk <damon.eccles@hotmail.com>
2025-12-07 15:35:59 +1000

    Completed day4 part 2, performance is pretty bad

--- day4/main.go
+++ day4/main.go
@@ -9,21 +9,56 @@ func main(){
 	start := time.Now()
 	advent := Advent_input()
 
+
+
+
+	fmt.Printf("%s\n\n", advent)
+	
+
+
 	Check_paper(advent)	
 
 	elapsed := time.Since(start)
 	fmt.Printf("Finished %d ms\n", elapsed.Milliseconds())
-
-
 
 }
 
 func Check_paper(advent string){
 	matrix := Create_matrix(advent)
 	count := 0
+	total := 0
+	changes := 0
+	
+	for {
+		changes = 0
+		count = 0
+		for i := 0; i < len(matrix); i++{
+			for k := 0; k < len(matrix[i]); k++{
+					
+				if (Check_border(matrix, k, i) == false && matrix[i][k] == '@'){
+					count++
+					changes++
+					fmt.Printf("x")
+					matrix[i][k] = '.'
+				} else{
+					fmt.Printf("%s", string(matrix[i][k]))
+				}
 
-	for i := 0; i < len()  
+			}
+			fmt.Printf("\n")
+		}
 
+		total += count
+
+		fmt.Println("")
+		fmt.Printf("Count: %d\n", total)
+		
+		// Look at the @ and check if they are the same as old matrix. 
+		if (changes == 0){
+			break
+		}
+
+	}
 }
 
 
@@ -32,6 +67,7 @@ func Create_matrix(advent string)[][]byte{
 	matrix := make([][]byte, len(line))
 
 	for i:= 0; i < len(line); i++{
+		matrix[i] = make([]byte, len(line[i]))
 		for k:= 0; k < len(line[i]); k++ {
 			matrix[i][k] = line[i][k]
 		}
@@ -39,3 +75,38 @@ func Create_matrix(advent string)[][]byte{
 
 	return matrix
 }
+
+func Check_border(matrix [][]byte, x int, y int)bool{
+	var count int = 0
+
+	for iterator := y-1; iterator < y+2; iterator++{
+		for kterator := x-1; kterator < x+2; kterator++{
+			if (iterator < 0){
+				continue
+			}
+			if (iterator >= len(matrix)){
+				continue
+			}
+			if (kterator < 0){
+				continue
+			}
+			if (kterator >= len(matrix[iterator])){
+				continue
+			}
+
+			if (iterator == y && kterator == x){
+				continue
+			}
+
+			if (matrix[iterator][kterator] == '@'){
+				count++
+			}
+
+		}
+	} 
+
+	if (count < 4){
+		return false
+	}
+	return true
+} 
